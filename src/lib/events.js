e096114739b151019f88fdbe91628b3efcf1775d
Abel Varghese <abelshinevarghese@gmail.com>
2025-12-14 20:39:23 +0400

    refactor:  created to keep data seperate

---
+++ src/lib/events.js
@@ -0,0 +1,45 @@
+const events = [
+    {
+      title: "SAE Supra",
+      date: "September 2-5, 2026",
+      location: "Buddh International Circuit, Noida",
+      image: "https://motoroctane.com/wp-content/uploads/2018/06/Maruti-Supra-grid-start-960x600.jpg.webp",
+      isPast: false,
+    },
+    {
+      title: "Formula Imperial",
+      date: "November 2026",
+      location: "",
+      image: "https://formulaimperial.com/wp-content/uploads/2019/03/Screen-Shot-2019-03-14-at-7.24.43-PM.png",
+      isPast: false,
+    },
+    {
+      title: "Formula Bharat",
+      date: "January 2027",
+      location: "",
+      image: "https://formulabharat.com/wp-content/uploads/2025/08/About-us1-768x512.jpg",
+      isPast: false,
+    },
+    {
+      title: "SAE Supra",
+      date: "1-5 October 2024",
+      location: "Buddh International Circuit, Noida",
+      image: "https://images.unsplash.com/photo-1736122054520-f17c2adfc7a8?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxmb3JtdWxhJTIwb25lJTIwcmFjZSUyMHRyYWNrfGVufDF8fHx8MTc2MDI2Njc4MXww&ixlib=rb-4.1.0&q=80&w=1080",
+      isPast: true,
+      result: "AIR 34",
+      summary: "AIR 34 | SAE Supra's debut event"
+    },
+    {
+      title: "SAE Supra",
+      date: "August 12-16, 2025",
+      location: "Buddh International Circuit, Noida",
+      image: "https://images.unsplash.com/photo-1735636134481-1c6ef7f8df49?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxyYWNpbmclMjBjaXJjdWl0JTIwYWVyaWFsfGVufDF8fHx8MTc2MDE4MjE0M3ww&ixlib=rb-4.1.0&q=80&w=1080",
+      isPast: true,
+      result: "AIR 30",
+      summary: "AIR 30 | Winner of cost event (most cost-effective student formula car in SUPRA history)"
+    },
+    
+    
+];
+
+export default events;  
